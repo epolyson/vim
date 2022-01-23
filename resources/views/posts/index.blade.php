5face4484ad288c8a9d7a6c4544a5d5b846762c6
epolyson <eikipolyson@gmail.com>
2022-01-23 11:00:03 +0900

    08-1修正

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+ <meta charset="utf-8">
+ <title>blog</title>
+ <meta name="description" content="ブログ投稿サイトです">
+ <meta name="keywords" content="ブログ,blog">
+ <link rel="stylesheet" href="sample.css">
+ <!--[if lt IE 9]>
+ <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+ <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+ <![endif]-->
+ <script src="sample.js"></script>
+ </head>
+ 
+ <body>
+ <!----- ヘッダー ----->
+ <header>
+     <h2 text-align=center>ブログ投稿サイト</h2>
+ </header>
+ 
+ <!----- ヘッダー END ----->
+ 
+ <!----- メインコンテンツ ----->
+ <article>
+ <h1></h1>
+ <section>
+ <h2>投稿一覧</h2>
+ <div　class = "wrapper">
+     <div class = "container-1">
+        <p>新着</p>
+     </div>
+     <div class = "container-2">
+         <ul>
+             <li>ブログ１</li>
+             <li>ブログ２</li>
+             <li>ブログ３</li>
+         </ul>
+     </div>
+ </div>
+ 
+ </section>
+ </article>
+ <!----- メインコンテンツ END ----->
+ 
+ <!----- フッター ----->
+ <footer>footer</footer>
+ <!----- フッター END ----->
+ </body>
+</html>
