611f8b99641024be1d2f842db00491fd352b0a2b
epolyson <eikipolyson@gmail.com>
2022-01-23 16:26:39 +0900

    08-2-2 finished.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts/index')->with(['posts' => $post->get()]);  
     }
 }
-
+?>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,50 +1,20 @@
 <!DOCTYPE html>
-<html lang="ja">
- <head>
- <meta charset="utf-8">
- <title>blog</title>
- <meta name="description" content="ブログ投稿サイトです">
- <meta name="keywords" content="ブログ,blog">
- <link rel="stylesheet" href="sample.css">
- <!--[if lt IE 9]>
- <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
- <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
- <![endif]-->
- <script src="sample.js"></script>
- </head>
- 
- <body>
- <!----- ヘッダー ----->
- <header>
-     <h2 text-align=center>ブログ投稿サイト</h2>
- </header>
- 
- <!----- ヘッダー END ----->
- 
- <!----- メインコンテンツ ----->
- <article>
- <h1></h1>
- <section>
- <h2>投稿一覧</h2>
- <div　class = "wrapper">
-     <div class = "container-1">
-        <p>新着</p>
-     </div>
-     <div class = "container-2">
-         <ul>
-             <li>ブログ１</li>
-             <li>ブログ２</li>
-             <li>ブログ３</li>
-         </ul>
-     </div>
- </div>
- 
- </section>
- </article>
- <!----- メインコンテンツ END ----->
- 
- <!----- フッター ----->
- <footer>footer</footer>
- <!----- フッター END ----->
- </body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>投稿一覧</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class = 'post'>
+                    <h2 class = 'title'>{{$post->title}}</h2>
+                    <p class = 'body'>{{$post->body}}</p>
+                </div>
+            @endforeach
+        </div>
+    </body>
 </html>
